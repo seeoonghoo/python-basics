325738f3aa6ce91f4ff1eda1864c7905711a0696
seeoonghoo <seeoonghoo@gmail.com>
2021-03-31 21:52:09 +0900

    practice 02

--- practice02/01.py
+++ practice02/01.py
@@ -1,8 +1,6 @@
 # 문제1. 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요.
 # 또, 디렉토리 경로명과 파일명을 분리하여 출력하세요.
 
-#아래꺼는 rsplit , 위에꺼는 split
-
 s = '/usr/local/bin/python'
 print(s.split('/'))
 for i in s.split('/'):
